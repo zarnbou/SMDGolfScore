ce2798b6d36a917995126427de0361be1d867f0a
Zarn Bou <Zarn Bou@RYOGA>
2017-06-30 15:56:58 +1000

    mew stuff

--- SMDGolfScore/www/scripts/index.js
+++ SMDGolfScore/www/scripts/index.js
@@ -8,16 +8,6 @@
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
     function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
     };
 
     function onPause() {
